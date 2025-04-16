488ac93a8024e220ad5ebcf3f2285b2157e8e89f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-17 00:08:50 +0200

    Update run.sh

--- my-ftp-browser/run.sh
+++ my-ftp-browser/run.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 set -e
 
 # Récupérer la configuration
 CONFIG_PATH=/data/options.json
 SERVER_CONFIG=/etc/ftpbrowser/server.json
 
-# Créer le répertoire de configuration s'il n'existe pas
+# Créer les répertoires nécessaires
 mkdir -p /etc/ftpbrowser
 mkdir -p /data/ftpbrowser
 mkdir -p /data/ftpbrowser/shares
@@ -19,8 +19,6 @@ bashio::log.level "$LOG_LEVEL"
 
 bashio::log.info "Démarrage de l'addon FTP Browser & Media Server"
 
-# Démarrer S6 Overlay
-exec /usr/bin/s6-svscan /etc/services.d
 
 
 
