b5dc0c5b050100aa41b0528234a69449924cc540
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-17 07:29:34 +0200

    Update server.py

--- my-ftp-browser/rootfs/usr/share/ftpbrowser/api/server.py
+++ my-ftp-browser/rootfs/usr/share/ftpbrowser/api/server.py
@@ -703,9 +703,8 @@ if __name__ == "__main__":
     clean_expired_shares()
     
     logger.info("Démarrage du serveur API FTP Browser")
-    HOST = os.getenv('FTP_BROWSER_HOST', '0.0.0.0')
-    PORT = int(os.getenv('FTP_BROWSER_PORT', 5000))
-    
+    HOST = '192.168.1.207'  # Remplacez par l'IP locale de votre machine
+    PORT = 5000
     run_simple(HOST, PORT, app, use_reloader=False)
 
 
